97e52b05c76e99faf9de3eafdb7c5af7212d24f6
maverick6130 <daror6130@gmail.com>
2020-10-28 12:39:05 +0530

    added script sending, error handling and testing needed with backend

--- ocde/src/app/editor/editor.component.ts
+++ ocde/src/app/editor/editor.component.ts
@@ -1,4 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { RunService } from '../run.service';
+import { RunInput } from '../run_input';
+import { RunOutput } from '../run_output';
 
 import * as ace from 'ace-builds';
 import 'ace-builds/src-noconflict/mode-c_cpp';
@@ -17,36 +20,54 @@ export class EditorComponent implements AfterViewInit {
   @ViewChild('codeEditor') private editor: ElementRef<HTMLElement>;
   private aceEditor : ace.Ace.Editor;
 
-  THEME : string = 'ace/theme/github';
-  LANG : string = 'ace/mode/c_cpp';
+  THEME : string = 'github';
+  LANG : string = 'c_cpp';
 
-  constructor() { }
+  code : string = '';
+  input : string = '';
+  output : string = '';
+
+  constructor(private runscript : RunService) { }
 
   ngAfterViewInit(): void {
-    ace.config.set("fontSize", "14px");
+    ace.config.set("fontSize", "16px");
     this.aceEditor = ace.edit(this.editor.nativeElement);
-    this.aceEditor.setTheme(this.THEME);
-    this.aceEditor.session.setMode(this.LANG);
+    this.aceEditor.setTheme('ace/theme/'+this.THEME);
+    this.aceEditor.session.setMode('ace/mode/'+this.LANG);
     this.aceEditor.session.setValue("#include<bits/stdc++.h>");
   }
 
-  public getCode() {
-    const code = this.aceEditor.getValue();
-    console.log(code);
+  public runCode() {
+      this.code = this.aceEditor.session.getValue();
+      const send : RunInput = {
+        script : this.code,
+        language : this.LANG,
+        input : this.input
+      };
+      console.log(send);
+      this.runscript.runScript(send).subscribe(receivied =>
+        {
+          if(receivied.success==true) {
+            this.output = receivied.output;
+          }   
+          else {
+            this.output = "Some Error Occured";
+          }
+        });
   }
 
   public updateTheme() {
-    this.aceEditor.setTheme(this.THEME);
+    this.aceEditor.setTheme('ace/theme/'+this.THEME);
   }
 
   public updateLang() {
-    this.aceEditor.session.setMode(this.LANG);
+    this.aceEditor.session.setMode('ace/mode/'+this.LANG);
     switch(this.LANG) {
-      case 'ace/mode/c_cpp': {
+      case 'c_cpp': {
         this.aceEditor.session.setValue("#include<bits/stdc++.h>");
         break;
       }
-      case 'ace/mode/python': {
+      case 'python': {
         this.aceEditor.session.setValue("import os");
         break;
       }
