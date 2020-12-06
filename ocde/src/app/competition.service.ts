0432365367ed0ceba05b225681cd730080737ce4
maverick6130 <daror6130@gmail.com>
2020-12-07 00:46:31 +0530

    added contest creation frontend, final service calls

--- ocde/src/app/competition.service.ts
+++ ocde/src/app/competition.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from 'rxjs';
 import { Comp } from './comp';
+import { Contest } from "./contest";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ import { Comp } from './comp';
 export class CompetitionService {
 
   postUrl = 'http://127.0.0.1:8000/api/-fill-here-';
+  getUrl = 'http://127.0.0.1:8000/api/-fill-here-';
 
   constructor(private http : HttpClient) { }
 
@@ -17,8 +19,12 @@ export class CompetitionService {
     headers : new HttpHeaders({'Content-Type':'application/json'})
   };
 
-  fetchCompetetions() : Observable<Comp> {
-    return this.http.post<Comp>(this.postUrl,this.httpOptions);
+  fetchCompetetions() : Observable<Comp[]> {
+    return this.http.get<Comp[]>(this.getUrl,this.httpOptions);
+  }
+
+  regContest(contest : Contest) : Observable<String> {
+    return this.http.post<String>(this.postUrl,contest,this.httpOptions);
   }
 
 }
