e9d86eab0bad3858ee8a225b9b179397fd142d25
maverick6130 <daror6130@gmail.com>
2020-12-07 23:08:17 +0530

    sync with backend, contests with C++ and python work

--- ocde/src/app/competition.service.ts
+++ ocde/src/app/competition.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from 'rxjs';
 import { Comp } from './comp';
-import { Contest } from "./contest";
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +12,8 @@ export class CompetitionService {
   postUrl = 'http://127.0.0.1:8000/api/contest/postcontest';
   getallUrl = 'http://127.0.0.1:8000/api/contest/getall';
   getUrl = 'http://127.0.0.1:8000/api/competition'
+  submitCodeUrl = 'http://127.0.0.1:8000/api/submit/code'
+  submitFileUrl = 'http://127.0.0.1:8000/api/submit/file'
 
   constructor(private http : HttpClient) { }
 
@@ -26,6 +27,14 @@ export class CompetitionService {
 
   fetchCompetitionbyId(index) : Observable<Comp> {
     return this.http.get<Comp>(`${this.getUrl}/${index}`);
+  }
+
+  submitCode(id,code) : Observable<String> {
+    return this.http.post<String>(`${this.submitCodeUrl}/${id}`,code);
+  }
+
+  submitFile(id,code) : Observable<String> {
+    return this.http.post<String>(`${this.submitFileUrl}/${id}`,code);
   }
 
 }
